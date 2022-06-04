9331de08b9b4b41840317a5404ccdf8e9e30ca7c
HassanZz <hassan.zamani.75@gmail.com>
2022-06-04 18:41:01 +0430

    recipe list added to home page

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
 //styles
+
 import "./RecipeList.css";
 
-const RecipeList = () => {
-  return <div></div>;
+const RecipeList = ({ recipes }) => {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="card">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipe/:${recipe.id}`}>Cook This</Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default RecipeList;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,10 @@
-import RecipeList from "../../components/RecipeList";
 import { useFetch } from "../../hooks/useFetch";
 
 //styles
 import "./Home.css";
+
+// components
+import RecipeList from "../../components/RecipeList";
 
 const Home = () => {
   const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
@@ -10,7 +12,7 @@ const Home = () => {
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && <RecipeList />}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 };

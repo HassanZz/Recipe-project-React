19f34cd7f6725fb6d34bcf1595b4bfdb3fbee647
HassanZz <hassan.zamani.75@gmail.com>
2022-06-05 00:34:13 +0430

    create component added

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,54 @@
+import { useState } from "react";
 //styles
 import "./Create.css";
 
 const Create = () => {
-  return <div>Create</div>;
+  const [method, setMethod] = useState("");
+  const [title, setTitle] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  console.log(title);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, cookingTime, method);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          ></input>
+        </label>
+        <label>
+          {/*  */}
+
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;

401154b375556d985bc3c21b82b10045092ce67e
HassanZz <hassan.zamani.75@gmail.com>
2022-06-04 18:27:12 +0430

    fetching data to Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,18 @@
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+
 //styles
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList />}
+    </div>
+  );
 };
 
 export default Home;

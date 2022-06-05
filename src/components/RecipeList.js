95462085e4411827b029966989ac4d5a1bbd7aba
HassanZz <hassan.zamani.75@gmail.com>
2022-06-05 18:02:23 +0430

    search not found added

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import "./RecipeList.css";
 
 const RecipeList = ({ recipes }) => {
+  if (recipes.length === 0) {
+    return <div className="error">No recipes to load...</div>;
+  }
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
@@ -11,7 +14,7 @@ const RecipeList = ({ recipes }) => {
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
-          <Link to={`/recipe/${recipe.id}`}>Cook This</Link>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
         </div>
       ))}
     </div>
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,8 +1,27 @@
+import { useLocation } from "react-router";
+import { useFetch } from "../../hooks/useFetch";
 //styles
+
 import "./Search.css";
 
+//component
+import RecipeList from "../../components/RecipeList";
+
 const Search = () => {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+  const url = "http://localhost:3000/recipes?q=" + query;
+  const { error, isPending, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipe including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 };
 
 export default Search;

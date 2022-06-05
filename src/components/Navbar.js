ad2c49e18d05f7a4b9e39d3e7e477ce06f76a9ce
HassanZz <hassan.zamani.75@gmail.com>
2022-06-05 18:57:40 +0430

    change theme added by using api context which was created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-
+import { useContext } from "react";
+import { ThemeContext } from "../Context/ThemeContext";
 //styles
 import "./Navbar.css";
 
@@ -7,8 +8,9 @@ import "./Navbar.css";
 import Searchbar from "./Searchbar";
 
 const Navbar = () => {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Coocking blog</h1>
